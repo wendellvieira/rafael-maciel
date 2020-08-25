c7ca9fe966f703b09c24a78e217d623e79df6ad7
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-08-25 16:13:22 -0300

    split menu

---
+++ fragments/menu.php
@@ -0,0 +1,82 @@
+<aside class='main-menu' >
+    <label class='btn-open-menu' for="open_menu">
+        <img src="assets/images/general/menu_open.png">
+    </label>
+    <input type="checkbox" id="open_menu" style="display: none;">
+
+    <nav class="cnt-items">
+        <label class='btn-open-menu' for="open_menu">
+            <img src="assets/images/general/menu_close.png">
+        </label>
+
+        <div class='cnt-items-menu pt-2 px-2'>
+            <a class='text-white' href="/">Home</a>
+            <ul class='text-blue-1 pl-none pb-1'>LATEST WORK</ul>
+
+            <a href="pages/nespresso.php">
+                <ul class='text-white f-22 pl-none' >Nespresso</ul>
+            </a>
+
+            <a href="pages/wikipedia.php">
+                <ul class='text-white f-22 pl-none' >Wikipedia</ul>
+            </a>
+            <a href="pages/pig.php">
+                <ul class='text-white f-22 pl-none' >Pig</ul>
+            </a>
+            <a href="pages/school-car.php">
+                <ul class='text-white f-22 pl-none' >SchoolCar</ul>
+            </a>
+            <a href="pages/xports.php">
+                <ul class='text-white f-22 pl-none' >XPorts</ul>
+            </a>
+
+            <a href="pages/rick-riordan.php">
+                <ul class='text-white f-22 pl-none' >Rick Riordan</ul>
+            </a>
+            <a href="pages/luxury-cars.php">
+                <ul class='text-white f-22 pl-none' >Luxury Cars</ul>
+            </a>
+            <a href="pages/alexandre-bispo.php">
+                <ul class='text-white f-22 pl-none' >Alexandre Bispo</ul>
+            </a>
+            <a href="pages/learn-ink.php">
+                <ul class='text-white f-22 pl-none' >Learn INK</ul>
+            </a>
+
+            <a href="pages/university-of-virginia.php">
+                <ul class='text-white f-22 pl-none' >University of Virginia Library</ul>
+            </a>
+            <a href="pages/jamiq.php">
+                <ul class='text-white f-22 pl-none' >JamiQ Dashboard</ul>
+            </a>
+            <a href="pages/guia-ir.php">
+                <ul class='text-white f-22 pl-none' >Guia IR</ul>
+            </a>
+            <a href="pages/plantao-de-vendas.php">
+                <ul class='text-white f-22 pl-none' >Plantão de Vendas</ul>
+            </a>
+            
+            <ul class="container row align-center space-between pl-none pt-2 pb-1">
+                <a href="" class="text-white" >About</a>
+
+                <a href="">
+                    <img src="assets/images/general/men_dribble.png" >
+                </a>
+                <a href="">
+                    <img src="assets/images/general/men_instagram.png" >
+                </a>
+                <a href="">
+                    <img src="assets/images/general/men_medium.png" >
+                </a>
+                <a href="">
+                    <img src="assets/images/general/men_linkedin.png" >
+                </a>
+                <a href="">
+                    <img src="assets/images/general/men_email.png" >
+                </a>
+
+            </ul>
+
+        </div>
+    </nav>
+</aside>
