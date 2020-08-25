84034ded85bcc66a526d23fb3fe371cfa20bc795
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-08-25 18:51:11 -0300

    footer

--- assets/configs.php
+++ assets/configs.php
@@ -1,3 +1,56 @@
 <?php
 
-    $_BASEDIR = "http://sandbox.kdcwordpress.com.br/rafael-maciel/";
+    $_PAGES = [
+        [
+            "url" => "/rafael-maciel/pages/plantao-de-vendas.php",
+            "title" => "Plantão de Vendas"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/guia-ir.php",
+            "title" => "Guia IR"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/jamiq.php",
+            "title" => "JamiQ Dashboard"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/university-of-virginia.php",
+            "title" => "University of Virginia"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/learn-ink.php",
+            "title" => "Learn INK"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/alexandre-bispo.php",
+            "title" => "Alexandre Bispo"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/luxury-cars.php",
+            "title" => "Luxury Cars"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/rick-riordan.php",
+            "title" => "Rick Riordan"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/xports.php",
+            "title" => "XPorts"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/school-car.php",
+            "title" => "School Car"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/pig.php",
+            "title" => "Pig"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/wikipedia.php",
+            "title" => "Wikipedia"
+        ],
+        [
+            "url" => "/rafael-maciel/pages/nespresso.php",
+            "title" => "Nespresso"
+        ]
+    ];
--- assets/js/fixed-header.js
+++ assets/js/fixed-header.js
@@ -5,9 +5,20 @@ $(document).ready( function() {
         $(new_elemente)
             .html( $(el).html() )
             .attr("href", `#${ $(el).attr("name") }`)    
-            .addClass("item-menu")    
+            .addClass("item-menu")
+            .click( function(ev){
+                let target = $(this).attr("href").replace("#", "");
+                let position = $( `a[name='${target}']` );
+                $('html, body').animate({ scrollTop: position.offset().top }, 1000);
+                return false;
+            }); 
+            
+        
 
-        items.push( new_elemente )        
+        items.push( new_elemente );        
     });
-    $("#cases-menu-target").html(items)
+    $("#cases-menu-target").html(items);
+
+
+
 });
