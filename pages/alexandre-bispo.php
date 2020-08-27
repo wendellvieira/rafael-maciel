8cc69c48e57d91e7d0e5b653c28e7347b04507cb
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-08-27 16:40:24 -0300

    fix alexandre bispo

--- pages/alexandre-bispo.php
+++ pages/alexandre-bispo.php
@@ -3,16 +3,13 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Rafael Maciel</title>
+        <title>Alexandre Bispo - Rafael Maciel</title>
 
-        <link rel="stylesheet" href="/rafael-maciel/assets/css/vars.css">
-        <link rel="stylesheet" href="/rafael-maciel/assets/css/ArgentCF/stylesheet.css">
-        <link rel="stylesheet" href="/rafael-maciel/assets/css/Gilroy/stylesheet.css">
-        <link rel="stylesheet" href="/rafael-maciel/assets/css/cases-style.css">
+        <?php require_once __DIR__ . "/../fragments/cases-includes.php"; ?>
 
     </head>
     <body>
-        <h1 class='small-title my-1'>RAFAEL MACIEL</h1>
+        <?php require_once __DIR__ . "/../fragments/cases-header.php"; ?>
 
         <section class="container bg-white mx-1" style="padding-top: 189px; margin-bottom: 150px;">
             <div class="gilroy-regular text-grey-3">
@@ -38,7 +35,7 @@
             </div>
         </section>
 
-        <section class="container pt-2 pb-4 bg-grey-1">
+        <section class="container pt-2 pb-4 mt-4 bg-grey-1">
             <h4 class="bordered-title">KPIS</h4>
 
             <h2 class="gilroy-bold text-blue-9 normal-title my-2 container row">
@@ -47,17 +44,19 @@
             </h2>
 
             <div class="container row align-stretch space-center">
-                <div class="col-2 bg-white mr-1 p-1">
+                <div class="col-3 bg-white mr-1 p-1">
                     Increase job opportunities.
                 </div>
-                <div class="col-2 bg-white p-1">
+                <div class="col-3 bg-white p-1">
                     Increase mentions of the website over the web.
                 </div>
             </div>
 
-
         </section>
 
+        <a name='meeting-the-user' class='map-cases-menu'>
+            01 - Meeting the user
+        </a>
         <section class="container bg-grid pt-1 pb-4 mt-2">
             <h2 class="text-blue-9 big-title argent-cf-bold text-center my-2">
                 01 <br>
@@ -138,43 +137,41 @@
                         <tr>
                             <th colspan="2">
                                 <div class="container row ">
-                                    <div class="col-5 text-white">PAIN</div>
-                                    <div class="col-2 text-white">X</div>
-                                    <div class="col-5 text-white">SOLUTION</div>
+                                    <div class="col-5 text-white normal-title">PAIN</div>
+                                    <div class="col-2 text-white normal-title">X</div>
+                                    <div class="col-5 text-white normal-title">SOLUTION</div>
                                 </div>
                             </th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td class="text-green text-left col-6">
-                                
+                            <td class="text-green text-left col-6">                                
                                 Developers apply for a job that they are not qualified to. It's a waste of time and frustrating for both sides.
-
                             </td>
                             <td>To show clearly what is the level of knowledge Alexandre has in each set of skills. </td>
                         </tr>
-                        <td class="text-green text-left">
-                                
-Some developers aren't good communicators, don't understand design process and are weak strategists (giving wrong deadlines, not helping designers understanding what's possible or how to keep things simple. 
-                        </td>
-                        <td>To show clearly what is the level of knowledge Alexandre has in each set of skills. </td>
-                    </tr>
-                    <td class="text-green text-left">
-                                
-                        When you see a portfolio it is difficult to know what the person did in the project or even how it was made.
-                    </td>
-                    <td>Every portfolio case has exactly what he did: coding language, frameworks used etc.  and a sample, so the recruiter is able to check the right version he developed. </td>
-                </tr>
+                        <tr>
+                            <td class="text-green text-left">                                
+                                Some developers aren't good communicators, don't understand design process and are weak strategists (giving wrong deadlines, not helping designers understanding what's possible or how to keep things simple. 
+                            </td>
+                            <td>To show clearly what is the level of knowledge Alexandre has in each set of skills. </td>
+                        </tr>
+                        <tr>
+                            <td class="text-green text-left">                                
+                                When you see a portfolio it is difficult to know what the person did in the project or even how it was made.
+                            </td>
+                            <td>Every portfolio case has exactly what he did: coding language, frameworks used etc.  and a sample, so the recruiter is able to check the right version he developed. </td>
+                        </tr>
                     </tbody>
-
                 </table>
-
             </div>
             
         </section>
             
-
+        <a name='user-interface' class='map-cases-menu'>
+            02 - User Interface
+        </a>
         <section class="container bg-grid pt-1 pb-4 mt-2">
             <h2 class="text-blue-9 big-title argent-cf-bold text-center my-2">
                 02 <br>
@@ -185,11 +182,13 @@ Some developers aren't good communicators, don't understand design process and a
 
         <section class="container pt-2 pb-4">
             <h4 class="bordered-title mb-3">HOME</h4>
-
             <img src="/rafael-maciel/assets/images/alexandre-bispo/alexandre_ui1.png">
+
+            <h4 class="bordered-title mb-3 mt-3">EXPANDED MENU</h4>
+            <img src="/rafael-maciel/assets/images/alexandre-bispo/alexandre_ui2.png">
         </section>
 
-        <div class="container bg-white pt-2 pb-4">
+        <section class="container bg-white pt-2 pb-4">
             <h4 class="bordered-title mb-3">IA AND STRATEGY</h4>
             <div class="container row space-evenly align-start mt-3">
                 <div class="col-4">
@@ -198,8 +197,8 @@ Some developers aren't good communicators, don't understand design process and a
             
                 <div class="col-3">
                     <p>
-                        01. The developer role
-Recruiters can see exactly what Alexandre did in the project: technologies, frameworks and a sample of the version that he actually coded. Therefore, they can feel more comfortable when deciding to hire him.
+                        <div class="gilroy-semibold-italic">01. The developer role</div> 
+                        Recruiters can see exactly what Alexandre did in the project: technologies, frameworks and a sample of the version that he actually coded. Therefore, they can feel more comfortable when deciding to hire him.
                     </p>
 
                 </div>
@@ -211,12 +210,14 @@ Recruiters can see exactly what Alexandre did in the project: technologies, fram
                 </div>
                 <div class="col-3">
                     <p>
-                        02. Years of experience
-As an experienced developer, showing years of experience is an advantage for him.
-
-03. Skill level
-This information is very important, because it avoids any misunderstanding or frustrations when hiring him and recruiters know exactly what to expect from his work. 
+                        <div class="gilroy-semibold-italic">02. Years of experience</div>
+                        As an experienced developer, showing years of experience is an advantage for him.
                     </p>
+                    <p>
+                        <div class="gilroy-semibold-italic">03. Skill level</div>
+                        This information is very important, because it avoids any misunderstanding or frustrations when hiring him and recruiters know exactly what to expect from his work. 
+                    </p>
+                    
                 </div>
 
             </div>
@@ -226,28 +227,85 @@ This information is very important, because it avoids any misunderstanding or fr
                 </div>
                 <div class="col-3">
                     <p>
-                        04. Blog
-I’ve encouraged him to start a blog. Besides showing passion about his career, he can learn more, spread his knowledge and engage with the coding community.  
+                        <div class="gilroy-semibold-italic">04. Blog</div>
+                        I’ve encouraged him to start a blog. Besides showing passion about his career, he can learn more, spread his knowledge and engage with the coding community.  
+                     </p>
+                </div>
+            </div>
+        </section>
 
-                    </p>
+        <section class="container pt-3 pb-2">
+            <h4 class="bordered-title">STYLE GUIDE</h4>
+        </section>
+        <section style="background-color: #0F0426;" class="container pt-3 pb-4 mx-1">
+            <div class="container width-1080 row space-evenly align-start mt-3">
+                <div class="col-3">
+                    <nav class="style-guide">
+                        <header class="gilroy-bold text-white">Colors</header>
+                        
+                        <ul class="gilroy-medium pl-none mt-1"> 
+                            <span class="text-white">interface color palette</span>
+                            <img src="/rafael-maciel/assets/images/alexandre-bispo/cores.png">
+                        </ul>
+                        <ul class="gilroy-medium pl-none mt-1"> 
+                            <span class="text-white">TYPOGRAPHY</span>
+                            <img src="/rafael-maciel/assets/images/alexandre-bispo/fonts.png">
+                        </ul>                        
+                    </nav>
+                </div>
+                <div class="col-3">
+                    <nav class="style-guide">
+                        <header class="gilroy-bold text-white text-white">ELEMENTS</header>
+                        
+                        <ul class="gilroy-medium pl-none mt-1"> 
+                            <span class="text-white">Navigation Items</span>
+                            <img src="/rafael-maciel/assets/images/alexandre-bispo/botoes.png">
+                        </ul>
+                        <ul class="gilroy-medium pl-none mt-1"> 
+                            <span class="text-white">Form items</span>
+                            <img src="/rafael-maciel/assets/images/alexandre-bispo/inputs.png">
+                        </ul>
+                        
+                        <header class="gilroy-bold text-white text-white">OTHER</header>
+                        <ul class="gilroy-medium pl-none mt-1"> 
+                            <img src="/rafael-maciel/assets/images/alexandre-bispo/outros.png">
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-3">
+                    <nav class="style-guide">
+                        <header class="gilroy-bold text-white">ICONS</header>
+                        
+                        <ul class="gilroy-medium pl-none mt-1"> 
+                            <img src="/rafael-maciel/assets/images/alexandre-bispo/icones.png">
+                        </ul>
+
+                    </nav>
                 </div>
 
             </div>
-        </div>
-        <section> sessão não feita </section>
-        <h4 class="bordered-title">USER INTERVIEW</h4>
-        <section class="container bg-grid pt-1 pb-4 mt-2 mx-none">
+
+        </section>
+
+       
+
+        <a name='final-words' class='map-cases-menu'>
+            04 - Final Words
+        </a>
+        <section class="container bg-grid pt-1 pb-4 mt-4 mx-none">
             <h2 class="text-blue-9 big-title argent-cf-bold text-center my-2">
                 03 <br>
                 <small class="text-blue-9 argent-cf-bold">Final Words</small>
             </h2>
             <div class="line-art"></div>
         </section>
-        <div class="container width-1080 row space-center">
+        <section class="container width-1080space-center py-4">
             <div class="col-8">
                 Since I know many designers and developers, who are often hiring developers, it was never so easy to recruit users for testing the website :). In the end, the main objective that even before launching the site I managed to fulfill was to encourage Alexandre to start writing, be engaged with the dev community and put more focus on his career.
             </div>
-        </div>
+        </section>
+
+        <?php require_once __DIR__ . "/../fragments/cases-footer.php"; ?>
 
     </body>
 </html>
