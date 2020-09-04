a2767c8caa56c346828f502c320141735e46eb6d
Wendell Vieira <wendell.vieiracunha@gmail.com>
2020-09-04 15:21:10 -0300

    fix pig

--- pages/pig.php
+++ pages/pig.php
@@ -3,16 +3,13 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Rafael Maciel</title>
+        <title>Pig - Rafael Maciel</title>
 
-        <link rel="stylesheet" href="/rafael-maciel/assets/css/vars.css">
-        <link rel="stylesheet" href="/rafael-maciel/assets/css/ArgentCF/stylesheet.css">
-        <link rel="stylesheet" href="/rafael-maciel/assets/css/Gilroy/stylesheet.css">
-        <link rel="stylesheet" href="/rafael-maciel/assets/css/cases-style.css">
+        <?php require_once __DIR__ . "/../fragments/cases-includes.php"; ?>
 
     </head>
     <body>
-        <h1 class='small-title my-1'>RAFAEL MACIEL</h1>
+        <?php require_once __DIR__ . "/../fragments/cases-header.php"; ?>
 
         <section class="container bg-white mx-1" style="padding-top: 189px; margin-bottom: 150px;">
             <div class="gilroy-regular text-grey-3">
@@ -37,23 +34,32 @@
                 <p>User Research (Survey, User and Stakeholder Interviews), Product Strategy, Persona, User Journey Map, Product Life Cycle, Information Architecture, User Interface, Style Guide, User Testing.</p>
             </div>
         </section>
-        <section class="container row space-evenly align-start mt-3">
-            <div class="col-5 mr-2 ml-1 pt-2">
-                <p class="mt-none medium-title">
-                    GR1D Hackathon
-                </p>
-                <p>
-                    Pig was developed during Hacka Gr1d 2019.
-                    The main sponsor, Gr1d, is an api marketplace for life insurance companies. For the hackathon, Gr1d invited 40 tech professionals to explore the apis and create disrupt products regarding investments and life insurance business.
-                    We got first place, yesss! and iPhone XRs for each member of the team, Wow. But the best of it is the opportunity that we’re having to develop the product with this lovely team.</p>
+
+        <section class="container my-3">
+            <div class="container width-1080 row space-evenly align-start">
+                <div class="col-5 mr-2 ml-1">
+                    <p class="mt-none normal-title text-blue-9 gilroy-bold">
+                        GR1D Hackathon
+                    </p>
+                    <p>
+                        Pig was developed during Hacka Gr1d 2019.
+                        The main sponsor, Gr1d, is an api marketplace for life insurance companies. For the hackathon, Gr1d invited 40 tech professionals to explore the apis and create disrupt products regarding investments and life insurance business.
+                        We got first place, yesss! and iPhone XRs for each member of the team, Wow. But the best of it is the opportunity that we’re having to develop the product with this lovely team.
+                    </p>
+                </div>
+                <div class="col-7 text-right">
+                    <img src="/rafael-maciel/assets/images/pig/Grupo 894.png">
+                    <small class="small-title">
+                        Willian Vale (developer), Maisa (developer), Beatriz Maselli (business), Diago Savio (marketing) and me. 
+                    </small>
+                </div>
             </div>
-            <div class="col-7 align-center row">
-                <img src="/rafael-maciel/assets/images/pig/Grupo 894.png">
-                <p class="small-title">Willian Vale (developer), Maisa (developer), Beatriz Maselli (business), Diago Savio (marketing) and me. </p>
-            </div>
-            
 
         </section>
+
+        <a name='discover' class='map-cases-menu'>
+            01 - Discover
+        </a>
         <section class="container bg-grid pt-1 pb-4 mt-2">
             <h2 class="text-blue-9 big-title argent-cf-bold text-center my-2">
                 01 <br>
@@ -61,119 +67,200 @@
             </h2>
             <div class="line-art"></div>
         </section> 
-        <section> a fazer </section>
-        <h4 class="bordered-title">USER INTERVIEW</h4>
-        <section class="container bg-grid pt-1 pb-4 mt-2">
+
+        <section class='container bg-white mx-1 pt-2 pb-4'>
+            <h4 class="bordered-title">USER INTERVIEW</h4>
+
+            <div class="width-1080 mt-3">
+                <b class="text-big text-blue-9 ">Every hour, 321 babies are born in Brazil.</b>
+                <p class='mb-2'>In a country that is facing an economic downturn in 2019, raising a child is extremely expensive and at the end of the month, there is no money left to think about the child future. So, how about helping all these parents to realize the dream of providing something they want for their kids future? Besides being a designer, I’m a father and I believe it’s something memorable to do.</p>
+                <b class="text-big text-blue-9 ">Brazilians are terrible in finance.</b>
+                <p>Our first research was to study the brazilian habits and knowledge level in finance. We discovered that Brazilians are choosing an investment with a very low return, Savings Account.</p>
+            </div>   
+
+            <div class="container row width-1080 mt-2">
+                <div class="col-4 normal-title text-blue-9">
+                    42% of Brazilians have investments
+                </div>
+                <div class="col-4 text-center">
+                    <img src="/rafael-maciel/assets/images/pig/pig_grafico1.png">
+                    <div class='small-title'>AMBIMA – Brazilian Investor X-Ray - 2nd Edition</div>
+                </div>
+                <div class="col-4">
+                    <table>
+                        <tr>
+                            <td class='text-big text-blue-4 pr-1'>04%</td>
+                            <td class='text-big text-blue-9'>Investment Funds</td>
+                        </tr>
+                        <tr>
+                            <td class='text-big' style='color: #A28E81'>05%</td>
+                            <td class='text-big text-blue-9'>Investment Funds</td>
+                        </tr>
+                        <tr>
+                            <td class='text-big text-grey-3'>06%</td>
+                            <td class='text-big text-blue-9'>Private pension</td>
+                        </tr>
+                        <tr>
+                            <td class='text-big text-blue-9 gilroy-bold'>88%</td>
+                            <td class='text-big text-blue-9 gilroy-bold'>Savings Account</td>
+                        </tr>
+                    </table>
+                    <small class='small-title text-red'>Investment with the worst return.</small>
+                </div>
+            </div>
+            <div class="grid gap-xl width-1080 mt-3">
+                <div class="col-4">
+                    <div class='normal-title gilroy-bold text-blue-9'>Bad Finance Habits</div>
+                    <p class='border-top-red'>They only invest the money left of the month. Rarely they invest in the first days of the month.</p>
+                </div>
+                <div class="col-4">
+                    <p class='border-top-red' style='margin-top: 52px'>They don’t have goals well defined.</p>
+                </div>
+                <div class="col-4">
+                    <p class='border-top-red' style='margin-top: 52px'>They think investing is bureaucratic and only invest in basic options.</p>
+                </div>
+                <div class="col-4">
+                    <p class='border-top-red' style='margin-top: 52px'>They prefer to buy something unnecessary than saving the money.</p>
+                </div>
+                <div class="col-4">
+                    <p class='border-top-red' style='margin-top: 52px'>They don’t have finance education.</p>
+                </div>
+            </div>
+
+        </section>
+
+        <a name='define' class='map-cases-menu'>
+            02 - Define
+        </a>
+        <section class="container bg-grid pt-1 pb-4">
             <h2 class="text-blue-9 big-title argent-cf-bold text-center my-2">
                 02 <br>
                 <small class="text-blue-9 argent-cf-bold">Define</small>
             </h2>
             <div class="line-art"></div>
         </section> 
-        <section> a fazer </section>
-        <h4 class="bordered-title">USER INTERVIEW</h4>
-        <div class="container bg-white pt-2 pb-4">
-            <div class="container bg-white pt-2 pb-4">
-                <h4 class="bordered-title mb-3">OPORTUNITY</h4>
-                <div class="container row space-evenly align-start mt-3">
-                    
-                    <div class="col-3">
-                        <p>
+
+        <section class='container mx-1'>
+            <img src="/rafael-maciel/assets/images/pig/pig_ideation.png">
+        </section>
+        <section class="container bg-blue-9 mx-1">
+            <div class="col-8 py-4 col-8">
+                <small class="text-blue-1">PROBLEM</small>
+                <div class="text-white normal-title">Brazilian parents are having a hard time paying their children’s bills, and consequently, forgetting about their kid’s future.</h3>
+            </div>
+        </section>
+        <section class='container mx-1 gap-xl' style='background-color: #B1C3D5'>
+            <div class="container width-1080 row py-2 space-center">
+                <div class="col-4 normal-title text-blue-9">
+                    How could they solve it?
+                </div>
+                <div class="col-2 container">
+                    <img src="/rafael-maciel/assets/images/pig/pig_icon_betterfinance.png"> 
+                    <span class='text-center small-title text-blue-9'>
+                        BETTER FINANCE <br>
+                        HABITS
+                    </span>                   
+                </div>
+                <div class="col-2 container">
+                    <img src="/rafael-maciel/assets/images/pig/pig_icon_help.png">
+                    <span class='text-center small-title text-blue-9'>
+                        THE HELP <br>
+                        OF RELATIVES
+                    </span>
+                </div>
+            </div>
+        </section>
+
+        <section class="container pt-2 pb-2">
+            <div class="container pt-2 pb-4">
+                <h4 class="bordered-title">OPORTUNITY</h4>
+                
+                <div class="grid width-1080 gap-xl mt-2">                    
+                    <div class="col-4">
+                        <p class='gilroy-bold text-big text-blue-9'>
                             Together we can do more.
                         </p>
                         <p>
                             Family, relatives, friends, there are many people that could help a kid. They just need to want to help.
-                        </p>
-                    
-    
+                        </p>    
                     </div>
                     <div class="col-4">
                         <img src="/rafael-maciel/assets/images/pig/pig_oportunity_img1.png">
                     </div>
     
                 </div>
-                <div class="container row space-evenly align-start mt-3">
-                    
-                    <div class="col-3">
-                        <p>
-                            Invest in the kids 
-                            future as a gift.
+
+                <div class="grid width-1080 gap-xl mt-3">                    
+                    <div class="col-4">
+                        <p class='gilroy-bold text-big text-blue-9'>
+                            Invest in the kids future as a gift.
                         </p>
                         <p>
                             Imagine a birthday of a 2 year old kid, that often ends with tons of toys that we don’t have no room to store them at our houses. Now imagine if people could give the most important gift for this kid, an investment in his future.
-                        </p>
-                        
+                        </p>                        
                     </div>
                     <div class="col-4">
                         <img src="/rafael-maciel/assets/images/pig/pig_oportunity_img2.png">
                     </div>
                 </div>
                    
-                   
-                    <section class="container bg-white mx-1 pt-2 pb-4 mt-2">
-                        <h4 class="bordered-title">PERSONA</h4>
-            
-                        <div class="container width-1080">
-                            <div class="col-12">
-                                <h2 class="medium-title text-blue-9">
-                                    Lucas
-                                    <small class="text-grey-3 small-title block gilroy-regular">
-                                        PARENT
-                                    </small>
-                                </h2>
-                            </div>
-            
-                            <div class="col-12">
-                                <div class="container row">
-                                    <div class="col-4 argent-cf-bold normal-title letter-045 text-blue-9 ">
-                                        “I work very hard to pay my kid’s bills.”
-                                    </div>
-                                    <div class="col-5 pl-1">
-                                        <span class="small-title">ABOUT</span>
-                                        <p>
-                                            Age: 32 <br>
-                                            Income: Medium <br>
-                                            Technology Proficiency: Medium - High <br>
-                                            Investments: a few money in savings account <br>
-                                        </p> 
-                                    </div>
-            
-                                </div>
-                            </div>
-            
-                            <div class="col-12 pt-1">                    
-                                <span class="small-title">CORE NEEDS</span>
-                                <div class="container row gap-md align-stretch mt-14">
-                                    <div class="col-3 bg-blue-9 text-white p-1">
-                                        Finance education.
-                                    </div>
-                                    <div class="col-3 bg-blue-9 text-white p-1">
-                                        To learn how to invest with a very tight budget.
-                                    </div>
-                                    <div class="col-3 bg-blue-9 text-white p-1">
-                                        To stop spending all the salary every month.
-                                    </div>
-                                    <div class="col-3 bg-blue-9 text-white p-1 position-relative">
-                                        <img class="img-top" style="margin-top: -276px;" src="/rafael-maciel/assets/images/pig/pig_persona.png">
-                                        To get better finance habits.
-                                    </div>
-            
-                                </div>
-                            </div>
-            
-                        </div>
-                    </section>
+            </div>
+        </section>
 
-    
-               
-    
+        <section class="container bg-white mx-1 pt-2 pb-4">
+            <h4 class="bordered-title">PERSONA</h4>
+
+            <div class="container width-1080">
+                <div class="col-12">
+                    <h2 class="medium-title text-blue-9">
+                        Lucas
+                        <small class="text-grey-3 small-title block gilroy-regular">
+                            PARENT
+                        </small>
+                    </h2>
                 </div>
-            </div>
 
+                <div class="col-12">
+                    <div class="container row">
+                        <div class="col-4 argent-cf-bold normal-title letter-045 text-blue-9 ">
+                            “I work very hard to pay my kid’s bills.”
+                        </div>
+                        <div class="col-5 pl-1">
+                            <span class="small-title">ABOUT</span>
+                            <p>
+                                Age: 32 <br>
+                                Income: Medium <br>
+                                Technology Proficiency: Medium - High <br>
+                                Investments: a few money in savings account <br>
+                            </p> 
+                        </div>
 
-            
+                    </div>
+                </div>
+
+                <div class="col-12 pt-1">                    
+                    <span class="small-title">CORE NEEDS</span>
+                    <div class="container row gap-md align-stretch mt-14">
+                        <div class="col-3 bg-blue-9 text-white p-1">
+                            Finance education.
+                        </div>
+                        <div class="col-3 bg-blue-9 text-white p-1">
+                            To learn how to invest with a very tight budget.
+                        </div>
+                        <div class="col-3 bg-blue-9 text-white p-1">
+                            To stop spending all the salary every month.
+                        </div>
+                        <div class="col-3 bg-blue-9 text-white p-1 position-relative">
+                            <img class="img-top" style="margin-top: -276px;" src="/rafael-maciel/assets/images/pig/pig_persona.png">
+                            To get better finance habits.
+                        </div>
+
+                    </div>
+                </div>
+
             </div>
-        </div>
+        </section>
+
         <section class="container pt-2 pb-2"> 
             <h4 class="bordered-title">USER JOURNEY MAP</h4>
 
